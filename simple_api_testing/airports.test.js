b7c430b59683fe2eb67c4bd44c7e9a80192f0110
Sami <sami.bencheikh@metropolia.fi>
2023-10-04 10:13:35 +0300

    update

---
+++ simple_api_testing/airports.test.js
@@ -0,0 +1,26 @@
+const { request, expect } = require("./config");
+
+describe("GET /airports", function () {
+  it("returns all airports, limited to 30 per page", async function () {
+    const response = await request.get("/airports");
+
+    expect(response.status).to.eql(200);
+    expect(response.body.data.length).to.eql(30);
+  });
+});
+
+describe("POST /airports/distance", function () {
+  it("calculates the distance between two airports", async function () {
+    const response = await request
+      .post("/airports/distance")
+      .send({ from: "KIX", to: "SFO" });
+
+    expect(response.status).to.eql(200);
+
+    const attributes = response.body.data.attributes;
+    expect(attributes).to.include.keys("kilometers", "miles", "nautical_miles");
+    expect(attributes.kilometers).to.be.closeTo(8692, 1);
+    expect(attributes.miles).to.be.closeTo(5397, 1);
+    expect(attributes.nautical_miles).to.be.closeTo(4690, 1);
+  });
+});
